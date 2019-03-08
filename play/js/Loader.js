58f967de885b8f6251495aa521cde91e08687188
paretoman <paretoman1@gmail.com>
2019-03-07 19:02:49 -0500

    midpoint  - better loader. getting multiple models to work in the same frame

--- play/js/Loader.js
+++ play/js/Loader.js
@@ -1,3 +1,39 @@
+// Each main() makes a new Loader.  The Loader is configured with an .onload and updated with .load.
+// Then all the Loaders call the LoaderManager to say what files they need and to give a countdown function _onAssetLoad
+// LoaderManager calls the countdown for each Loader each time an image loads.
+// When the countdown reaches 0, the .onload's run
+
+
+var LoaderManager = new function() {
+	var self = this;
+	var alreadyLoaded = []
+	var alreadyRequested = []
+	var onloads = {} // list of callbacks to do once the image loads
+	
+	self.request = function(src,f) {
+		if (alreadyLoaded.includes(src)) {
+			f() // do the call back now
+		} else if (alreadyRequested.includes(src)) {
+			onloads[src].push(f)  // do the callback later
+		} else {
+			alreadyRequested.push(src)
+			var img = new Image();
+			img.src = src;
+			img.onload = function() {loaded(src)} // send the network request for the image.
+			onloads[src] = []
+			onloads[src].push(f) // do the callback later
+		}
+		
+	}
+	function loaded(src) {
+		alreadyLoaded.push(src)
+		for (var i=0; i < onloads[src].length; i++) {
+			onloads[src][i]() // do the callback
+		}
+	}
+}
+
+
 function Loader() {
 	var self = this;
 
@@ -18,12 +54,10 @@ function Loader() {
 	
 		// Load 'em all
 		for(var i=0;i<imagePaths.length;i++){
-			var img = new Image();
-			img.onload = _onAssetLoad;
-			img.src = imagePaths[i];
+			// send a request to LoaderManager to get an image and call back when it's done
+			LoaderManager.request(imagePaths[i], _onAssetLoad)
 		}
 	
 	};
 
 }
-window.onload = Loader.windowonload
