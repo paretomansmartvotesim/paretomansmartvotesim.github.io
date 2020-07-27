c884a266965c999fb20454528f15f5d87be003fd
paretoman <paretoman1@gmail.com>
2020-07-26 23:28:48 -0400

    update build scripts

--- script/build.bat
+++ script/build.bat
@@ -1,5 +1,1 @@
-REM change the jekyll environment.  Just change it locally for this script.
-
-SETLOCAL
-SET JEKYLL_ENV=production
 bundle exec jekyll build
---
+++ script/prod.bat
@@ -0,0 +1,5 @@
+REM change the jekyll environment.  Just change it locally for this script.
+
+SETLOCAL
+SET JEKYLL_ENV=production
+bundle exec jekyll build
